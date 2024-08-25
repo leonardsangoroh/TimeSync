09215fded19bfb8fcdbb6fc0a185d786887cc6fb
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-08-25 23:50:14 +0300

    feat: design main screen

--- TimeSync/ContactTableViewCell.swift
+++ TimeSync/ContactTableViewCell.swift
@@ -24,6 +24,7 @@ class ContactTableViewCell: UITableViewCell {
         addSubview(isWorkingStatus)
         
         configureImageView()
+        configureIsWorkingStatus()
         configureNameLabel()
         configureCountryLabel()
         configureLocalTimeLabel()
@@ -39,7 +40,7 @@ class ContactTableViewCell: UITableViewCell {
     func set(contact: Contact) {
         profilePhoto.image = contact.profilePhoto
         nameLabel.text = contact.name
-        localTimeLabel.text = contact.localTime
+        localTimeLabel.text = contact.localTime + "hrs"
         countryLabel.text = contact.country
         
         // set isWorkingStatus
@@ -60,20 +61,29 @@ class ContactTableViewCell: UITableViewCell {
     func configureNameLabel() {
         nameLabel.numberOfLines = 0
         nameLabel.adjustsFontSizeToFitWidth = true
+        nameLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        
     }
     
     
     func configureLocalTimeLabel() {
         localTimeLabel.numberOfLines = 1
         localTimeLabel.adjustsFontSizeToFitWidth = true
+        localTimeLabel.textColor = .secondaryLabel
+        localTimeLabel.font = UIFont.systemFont(ofSize: 15, weight: .regular)
     }
     
     
     func configureCountryLabel(){
         countryLabel.numberOfLines = 1
         countryLabel.adjustsFontSizeToFitWidth = true
+        countryLabel.textColor = .secondaryLabel
+        countryLabel.font = UIFont.systemFont(ofSize: 15)
     }
     
+    func configureIsWorkingStatus() {
+        isWorkingStatus.layer.cornerRadius = 5
+    }
     
     func setViewConstraints() {
         profilePhoto.translatesAutoresizingMaskIntoConstraints = false
@@ -83,26 +93,27 @@ class ContactTableViewCell: UITableViewCell {
         isWorkingStatus.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            profilePhoto.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+            profilePhoto.topAnchor.constraint(equalTo: self.topAnchor, constant: 3),
             profilePhoto.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
             profilePhoto.heightAnchor.constraint(equalToConstant: 60),
             profilePhoto.widthAnchor.constraint(equalToConstant: 60),
             
-            isWorkingStatus.topAnchor.constraint(equalTo: profilePhoto.bottomAnchor, constant: 5),
+            isWorkingStatus.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10),
             isWorkingStatus.widthAnchor.constraint(equalTo:profilePhoto.widthAnchor),
             isWorkingStatus.heightAnchor.constraint(equalToConstant: 10),
+            isWorkingStatus.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10),
             
             nameLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
-            nameLabel.heightAnchor.constraint(equalToConstant: 20),
+            nameLabel.heightAnchor.constraint(equalToConstant: 30),
             nameLabel.leadingAnchor.constraint(equalTo: profilePhoto.trailingAnchor, constant: 10),
             
-            countryLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10),
+            countryLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 2),
             countryLabel.heightAnchor.constraint(equalToConstant: 20),
             countryLabel.leadingAnchor.constraint(equalTo: profilePhoto.trailingAnchor, constant: 10),
             
-            localTimeLabel.topAnchor.constraint(equalTo: countryLabel.bottomAnchor, constant: 10),
+            localTimeLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
             localTimeLabel.heightAnchor.constraint(equalToConstant: 20),
-            localTimeLabel.leadingAnchor.constraint(equalTo: isWorkingStatus.trailingAnchor, constant: 10)
+            localTimeLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -10)
         ])
     }
     
--- TimeSync/MainTableViewController.swift
+++ TimeSync/MainTableViewController.swift
@@ -8,82 +8,46 @@
 import UIKit
 
 class MainTableViewController: UITableViewController {
+    
+    var contacts: [Contact] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        contacts = fetchData()
+        print(contacts.count)
+        navigationController?.navigationBar.prefersLargeTitles = true
+        configureTableView()
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return contacts.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Contact", for: indexPath) as! ContactTableViewCell
+        let contact = contacts[indexPath.row]
+        cell.set(contact: contact)
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    
+    
+    func configureTableView(){
+        tableView.rowHeight = 66
+        tableView.register(ContactTableViewCell.self, forCellReuseIdentifier: "Contact")
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    
+    func fetchData() -> [Contact] {
+        let contactOne = Contact(profilePhoto: UIImage(named: "Lee")!, name: "Lee Leonard", localTime: "2300", country: "KENYA")
+        let contactTwo = Contact(profilePhoto: UIImage(named: "Lee")!, name: "Lee Leonard", localTime: "2300" , country: "KENYA")
+        let contactThree = Contact(profilePhoto: UIImage(named: "Lee")!, name: "Lee Leonard", localTime: "2300" , country: "KENYA")
+        
+        return [contactOne, contactTwo, contactThree]
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
