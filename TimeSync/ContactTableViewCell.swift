be2a9bee19f36278d0aedc54c567d8c1c6a9accd
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-08-25 22:41:36 +0300

    feat: create contact model

---
+++ TimeSync/ContactTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ContactTableViewCell.swift
+//  TimeSync
+//
+//  Created by Lee Sangoroh on 25/08/2024.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TimeSync/Model/Contact.swift
@@ -0,0 +1,15 @@
+//
+//  Contact.swift
+//  TimeSync
+//
+//  Created by Lee Sangoroh on 25/08/2024.
+//
+
+import UIKit
+
+struct Contact {
+    var profilePhoto: UIImage
+    var name: String
+    var localTime: String
+    var country: String
+}
