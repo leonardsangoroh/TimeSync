b7c7a9bd3210beb797943c51168d0d895e2e2d1f
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2024-09-01 14:33:33 +0300

    feat: connect to gemini api

--- TimeSync/ViewControllers/LLMViewController.swift
+++ TimeSync/ViewControllers/LLMViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleGenerativeAI
 
 class LLMViewController: UIViewController {
 
@@ -13,17 +14,25 @@ class LLMViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        Task {
+            await firstPrompt()
+        }
     }
     
+    func firstPrompt () async {
+        let generativeModel =
+          GenerativeModel(
+            // Specify a Gemini model appropriate for your use case
+            name: "gemini-1.5-flash",
+            // Access your API key from your on-demand resource .plist file (see "Set up your API key"
+            // above)
+            apiKey: GeminiAPIKey.default
+          )
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let prompt = "Write a story about a magic backpack."
+        let response = try! await generativeModel.generateContent(prompt)
+        if let text = response.text {
+          print(text)
+        }
     }
-    */
-
 }
